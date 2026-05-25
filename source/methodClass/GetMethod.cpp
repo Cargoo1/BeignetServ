60e5529d1ee7b6aa73165fb0d142fb79a66b89e9
barmarti <RVTEL-git>
2026-05-25 10:30:33 +0200

    finished get methd

--- source/methodClass/GetMethod.cpp
+++ source/methodClass/GetMethod.cpp
@@ -10,14 +10,25 @@ GetMethod::~GetMethod() {};
 
 HttpResponse &GetMethod::executeMethod() {
 	HttpResponse ret(200);
-	std::string path = NormalizePath(this->_context.location.getRoot()) + NormalizePath(this->_context.location.getPath());
-	// std::string path = NormalizePath(this->_context.location.getRoot()) + NormalizePath(this->_context.request.getHeader().getTargetResource());
+	std::string path = NormalizePath(this->_context.location.getRoot()) + NormalizePath(this->_context.request.getHeader().getTargetResource());
+	// std::string path = NormalizePath(this->_context.location.getRoot()) + NormalizePath(this->_context.location.getPath());
 	struct stat path_stat;
 	if (stat(path.c_str(), &path_stat) < 0) {
 		ret.setStatusCode(404);
 		return (ret);
 	}
-	if (S_ISREG(path_stat.st_mode)) {
-		ret = executeMethod();
+	if (S_ISDIR(path_stat.st_mode)) {
+		path += "/index.html";
+		if (stat(path.c_str(), &path_stat) < 0) {
+			ret.setStatusCode(404);
+			return (ret);
+		}
 	}
+	if (!ret.setBodyFromFile(path)) {
+		ret.setStatusCode(403);
+		return (ret);
+	}
+	ret.setContentType(this->getContentType(path));
+	ret.addContentLength();
+	return (ret);
 }
