b3d16a65a15a30bac8a7c035194bceab10b1d5bb
barmarti <RVTEL-git>
2026-06-02 15:33:12 +0200

    x

--- source/methodClass/PostMethod.cpp
+++ source/methodClass/PostMethod.cpp
@@ -9,96 +9,9 @@
 #include <ctime>
 #include <sys/stat.h>
 
-
-
-
-
-// template <typename T> std::string toStr(T nbr) {
-// 	std::ostringstream os;
-// 	os << nbr;
-// 	std::string ret = os.str();
-// 	return (ret);
-// }
-
-// namespace { std::string generateTimestampFilename(std::string filename){
-// 	std::string::size_type pos = filename.find(".");
-// 	std::cout << filename << std::endl;
-// 	if (pos == std::string::npos) {
-// 		std::cout << "Warning: not a file" << std::endl;
-// 		return (filename);
-// 	}
-// 	std::string tmp = filename.substr(0, pos);
-// 	std::time_t resultTime = std::time(0);
-// 	std::string timestamp = toStr(resultTime);
-// 	std::string newFileName = tmp + "-" + timestamp;
-// 	tmp = filename.substr(pos);
-// 	newFileName += tmp;
-// 	return (newFileName);
-// } }
-
-// namespace { bool createFile(const std::string &fileName, const std::string &body) {
-// 	std::ofstream ofs;
-// 	ofs.open(fileName.c_str());
-// 	if (!ofs.is_open()) {
-// 		std::cerr << "creating " << fileName << " failed" << std::endl;
-// 		return (false);
-// 	}
-// 	ofs.write(body.c_str(), body.size());
-// 	ofs.close();
-// 	return (true);
-// } }
-
-// namespace { bool postFile(std::string path, const std::string &body){
-// 	std::size_t found = path.find_last_of("/\\");
-// 	std::string newPath = path.substr(0, found);
-// 	std::istringstream iss(newPath);
-// 	std::string file = path.substr(found+1);
-// 	std::string part, current_path = "";
-// 	struct stat path_stat;
-
-
-// 	if (found != std::string::npos) {
-// 		while (std::getline(iss, part, '/')) {
-// 			std::cout << part << std::endl;
-// 			if (part.empty())
-// 				continue;
-// 			current_path += part;
-// 			if (stat(current_path.c_str(), &path_stat) != 0) {
-// 				if (mkdir(current_path.c_str(), S_IRWXU) != 0) {
-// 					std::cerr << "Post method mkdir failed" << std::endl;
-// 					return (false);
-// 				}
-// 			}
-// 			current_path += "/";
-// 		}
-// 	}
-// 	if (!file.empty()) {
-// 		if (!current_path.empty())
-// 			current_path += "/";
-// 		current_path += file;
-// 		if (stat(current_path.c_str(), &path_stat) != 0)
-// 			file = generateTimestampFilename(file);
-// 		if (!createFile(current_path, body)) {
-// 			return (false);
-// 		}
-// 	}
-// 	return (true);
-// } }
-
-
-
-
-
-
-namespace { std::size_t toSizeT(const std::string &str) {
-	std::stringstream ss(str);
-	std::size_t ret;
-	ss >> ret;
-	return (ret);
-} }
-
 namespace { std::string generateTimestampFilename(std::string filename){
 	std::string::size_type pos = filename.find(".");
+	std::cout << filename << std::endl;
 	if (pos == std::string::npos) {
 		std::cout << "Warning: not a file" << std::endl;
 		return (filename);
@@ -112,27 +25,10 @@ namespace { std::string generateTimestampFilename(std::string filename){
 	return (newFileName);
 } }
 
-
-namespace { bool isFile(const std::string &str) {
-	std::string::size_type pos = str.find(".");
-	if (pos == std::string::npos) {
-		return (false);
-	}
-	std::string compare = str.substr(pos); 
-	if (compare == ".txt" || compare == ".png" || compare == ".jpg" || compare == ".pdf")
-		return (true);
-	return (false);
-} }
-
 namespace { bool createFile(const std::string &fileName, const std::string &body) {
 	std::ofstream ofs;
-	struct stat path_stat;
-	if (stat(fileName.c_str(), &path_stat) == 0) {
-		std::cerr << fileName << " exist already" << std::endl;
-		return (false);
-	}
 	ofs.open(fileName.c_str());
-	if (!ofs.is_open()) {
+	if (!ofs.is_open(), std::ios::binary) {
 		std::cerr << "creating " << fileName << " failed" << std::endl;
 		return (false);
 	}
@@ -142,21 +38,49 @@ namespace { bool createFile(const std::string &fileName, const std::string &body
 } }
 
 namespace { bool postFile(std::string path, const std::string &body){
-	std::istringstream iss(path);
+	std::size_t found = path.find_last_of("/\\");
+	std::string newPath = path.substr(0, found);
+	std::istringstream iss(newPath);
+	std::string file = path.substr(found+1);
 	std::string part, current_path = "";
 	struct stat path_stat;
-	const std::size_t size = 1024; 
 
-	while (std::getline(iss, part, '/')) {
-		current_path += part;
-		if (stat(current_path.c_str(), &path_stat) != 0) {
-			if (mkdir(current_path.c_str(), S_IRWXU) != 0) {
-				std::cerr << "Post method mkdir failed" << std::endl;
-				return (false);
+
+	if (found != std::string::npos) {
+		while (std::getline(iss, part, '/')) {
+			std::cout << part << std::endl;
+			if (part.empty())
+				continue;
+			current_path += part;
+			if (stat(current_path.c_str(), &path_stat) != 0) {
+				if (mkdir(current_path.c_str(), S_IRWXU) != 0) {
+					std::cerr << "Post method mkdir failed" << std::endl;
+					return (false);
+				}
 			}
+			current_path += "/";
+		}
+	}
+	if (!file.empty()) {
+		std::string tmp(current_path);
+		current_path += file;
+		int ret = stat(current_path.c_str(), &path_stat);
+		if (ret == 0)
+			file = generateTimestampFilename(file);
+		tmp += file;
+		current_path = tmp;
+		if (!createFile(current_path, body)) {
+			return (false);
 		}
 	}
 	return (true);
+} }
+
+namespace { std::size_t toSizeT(const std::string &str) {
+	std::stringstream ss(str);
+	std::size_t ret;
+	ss >> ret;
+	return (ret);
 } }
 
 PostMethod::PostMethod(ExecutionContext &context) : HttpMethod(context) {};
